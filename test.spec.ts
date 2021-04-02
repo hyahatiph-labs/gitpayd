0452d528d546fcaad31b00d0d80a56fb72eedc15
Nigel Christian <nigel.l.christian@gmail.com>
2021-04-02 07:38:11 -0400

    add test for collaborators

--- test.spec.ts
+++ test.spec.ts
@@ -1,20 +1,30 @@
-import { splitter } from './src/noops';
+import { splitter, validateCollaborators } from './src/noops';
 import { strict as assert } from 'assert';
+import { AuthorizedRoles } from './src/setup';
 
-const BODY = "Bounty: 100000";
-const INVALID_BODY = "Bountis100000";
-const DELIMITER = "Bounty:";
-const EXPECTED_VALUE = 100000;
+const BODY: string = "Bounty: 100000";
+const INVALID_BODY: string = "Bountis100000";
+const DELIMITER: string = "Bounty:";
+const EXPECTED_BOUNTY: number = 100000;
 
-describe('Validate', () => {
-  describe('Result', () => {
-    it('should return amount for parsing bounty', () => {
-      const check = splitter(BODY, DELIMITER);
-      assert.strictEqual(parseInt(check, 10), EXPECTED_VALUE);
-    });
-    it('should return null on invalid parsing', () => {
-      const check = splitter(INVALID_BODY, DELIMITER);
-      assert.strictEqual(check, null);
-    });
+describe('Test helper function for data parsing', () => {
+  it('should return amount for parsing bounty', () => {
+    const check = splitter(BODY, DELIMITER);
+    assert.strictEqual(parseInt(check, 10), EXPECTED_BOUNTY);
+});
+  it('should return null on invalid parsing', () => {
+    const check = splitter(INVALID_BODY, DELIMITER);
+    assert.strictEqual(check, null);
+  });
+});
+
+describe('Test helper function for author validation', () => {
+  it('should return true for owner', () => {
+    const check = validateCollaborators(AuthorizedRoles.OWNER);
+    assert.strictEqual(check, true);
+  });
+  it('should return true for collaborator', () => {
+    const check = validateCollaborators(AuthorizedRoles.COLLABORATOR);
+    assert.strictEqual(check, true);
   });
 });
